01936384784709c62646df24d767ddcb3074cd38
aa12gq <aa12gq@gmail.com>
2025-10-24 09:07:38 +0800

    Clean up formula format

--- Formula/stack-sync.rb
+++ Formula/stack-sync.rb
@@ -5,7 +5,7 @@ class StackSync < Formula
   sha256 "e8bdf7ef2aac0e4b32ff51f30fc6cdf392b4eddfe6d4057d8a3a09a9067e5721"
   version "1.1.4"
   license "MIT"
-
+  
   head "https://github.com/aa12gq/stack-file-sync-intellij.git", branch: "main"
 
   def install
