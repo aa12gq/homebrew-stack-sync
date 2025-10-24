168d0718e8ae4ae915eb00802b1a00cf63cd8e76
aa12gq <aa12gq@gmail.com>
2025-10-24 09:04:43 +0800

    Update stack-sync to v1.1.4 with pre-compiled binaries

--- Formula/stack-sync.rb
+++ Formula/stack-sync.rb
@@ -3,6 +3,7 @@ class StackSync < Formula
   homepage "https://github.com/aa12gq/stack-file-sync-intellij"
   url "https://github.com/aa12gq/stack-file-sync-intellij/releases/download/v1.1.4/stack-sync-darwin-amd64.tar.gz"
   sha256 "e8bdf7ef2aac0e4b32ff51f30fc6cdf392b4eddfe6d4057d8a3a09a9067e5721"
+  version "1.1.4"
   license "MIT"
   head "https://github.com/aa12gq/stack-file-sync-intellij.git", branch: "main"
 
