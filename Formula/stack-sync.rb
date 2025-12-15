80c7820591a5fddc644841182005af85b99812da
aa12gq <aa12gq@gmail.com>
2025-12-15 14:33:44 +0800

    stack-sync 1.2.0 (source build from head commit)

--- Formula/stack-sync.rb
+++ Formula/stack-sync.rb
@@ -6,14 +6,17 @@ class StackSync < Formula
 
   head "https://github.com/aa12gq/stack-file-sync-intellij.git", branch: "main"
 
-  url "https://github.com/aa12gq/stack-file-sync-intellij/archive/refs/tags/v1.2.0.tar.gz"
-  sha256 "4857f6d23b727bd07aab9bf73d9569aa77ca0db1a4d8448e078819d2a4fb489f"
+  url "https://github.com/aa12gq/stack-file-sync-intellij/archive/700d2080b92a3996e730fd54cd072ed101dac37e.tar.gz"
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
 
   depends_on "go" => :build
 
   def install
     cd "stack-sync-cli" do
-      system "go", "build", "-o", bin/"stack-sync", "./cmd/stack-sync"
+      system "go", "build",
+             "-ldflags", "-X main.Version=1.2.0",
+             "-o", bin/"stack-sync",
+             "./cmd/stack-sync"
     end
   end
 
